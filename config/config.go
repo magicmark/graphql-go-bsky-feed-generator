916edfcdb877e5ef22842aac14fc05b3b720fdaa
Mark Larah <mark@larah.me>
2024-11-25 16:26:44 -0600

    clean up config

--- config/config.go
+++ config/config.go
@@ -2,14 +2,3 @@ package config
 
 const HOSTNAME = "graphql-bsky-feed2.larah.dev"
 const FEED_URI = "at://did:plc:gqsp67ejj64fau2rp4wb6sj4/app.bsky.feed.generator/graphql"
-
-
-// ═ markl@GQXP9G4HJM: graphql-bsky-feed on  main  via 🐍 v3.13.0 (venv) 
-// ╚═ ♪ ./publish_feed.py
-// Successfully published!
-// Feed URI (put in "GRAPHQL_URI" env var): at://did:plc:gqsp67ejj64fau2rp4wb6sj4/app.bsky.feed.generator/markltesting2
-
-// ╔═ markl@GQXP9G4HJM: graphql-bsky-feed on  main  via 🐍 v3.13.0 (venv) 
-// ╚═ ♪ ./publish_feed.py
-// Successfully published!
-// Feed URI (put in "GRAPHQL_URI" env var): at://did:plc:gqsp67ejj64fau2rp4wb6sj4/app.bsky.feed.generator/graphql
